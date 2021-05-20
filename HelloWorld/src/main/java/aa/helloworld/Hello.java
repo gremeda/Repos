75ceb49e679323704ef8213e5b0cfc069ee2b42f
Basit Ali <gremeda@hotmail.com>
2021-05-20 18:50:18 -0400

    Hello, World in NetBeans

---
+++ HelloWorld/src/main/java/aa/helloworld/Hello.java
@@ -0,0 +1,16 @@
+/**
+ *@author Abdulbasit Ali
+ *email: gremeda@hotmail.com
+ *date: 2021-05-20
+ *purpose: My first Hello World program in an IDE
+ */
+// AA
+package aa.helloworld;
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Kishore Pashindla");
+    }  
+}
